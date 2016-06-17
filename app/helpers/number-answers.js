a50826bf7f22c19cf83bfce1228853e53217cd4f
Nick <nick.lyman@gmail.com>
2016-06-17 09:32:22 -0700

    Fix styling for glyphicons and add simple key to home page.

--- app/helpers/number-answers.js
+++ app/helpers/number-answers.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 export function numberAnswers(params) {
+  //Getting the first item from the question object and calling it question
   var question = params[0];
 
+  //glyphicons displayed next to the question if either of the two conditions below are met
   if (question.get('answers').get('length') >= 3) {
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-education"></span>')
   } else if (question.get('answers').get('length') === 0) {
