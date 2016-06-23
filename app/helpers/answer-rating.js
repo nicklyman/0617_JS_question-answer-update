d95d3b31bc682a58f5fc9dc239fccbe87ba9c032
Nick <nick.lyman@gmail.com>
2016-06-22 18:32:08 -0700

    Fix star glyph not showing using helper.

--- app/helpers/answer-rating.js
+++ app/helpers/answer-rating.js
@@ -2,12 +2,12 @@ import Ember from 'ember';
 
 //helper for displaying the star rating that will be used for sorting answers as a computed property
 export function answerRating(params) {
-  var answer = params[0];
+  var rating = params[0];
   var star = "Not rated yet";
 
-  if ('rating' !== "") {
+  if (rating !== "") {
     star = "";
-    for (var i = 0; i < 5 && i < 'rating'; i++) {
+    for (var i = 0; i < 5 && i < rating; i++) {
       star = star + '<span class="glyphicon glyphicon-star-empty star-glyph"></span>';
     }
   }
@@ -15,3 +15,16 @@ export function answerRating(params) {
 }
 
 export default Ember.Helper.helper(answerRating);
+
+
+//Not sure why this didn't work
+
+// var answer = params[0];
+// var star = "Not rated yet";
+//
+// if (answer.get('rating') !== "") {
+//   star = "";
+//   for (var i = 0; i < 5 && i < answer.get('rating'); i++) {
+//     star = star + '<span class="glyphicon glyphicon-star-empty star-glyph"></span>';
+//   }
+// }
